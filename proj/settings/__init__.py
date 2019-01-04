1bff6ddb00f8a453377cf8946edb6809f2fa3788
Ryan Parker <rparker@indeed.com>
2019-01-04 13:37:49 -0600

    try to fix static file serving

--- proj/settings/__init__.py
+++ proj/settings/__init__.py
@@ -198,8 +198,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # proj/static/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # proj/static
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'assets')  # Where all static files are collected to be served.
 
 
 # Google RECAPTCHA
