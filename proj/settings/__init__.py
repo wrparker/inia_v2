5b4920f7677dc5ccfc50fd464817076d023cac44
Ryan Parker <rparker@indeed.com>
2019-01-04 13:18:04 -0600

    editing deployment

--- proj/settings/__init__.py
+++ proj/settings/__init__.py
@@ -198,9 +198,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')  # proj/static/
-                    ]
-STATIC_ROOT = STATICFILES_DIRS[0]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # proj/static/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # proj/static
 
 
 # Google RECAPTCHA
