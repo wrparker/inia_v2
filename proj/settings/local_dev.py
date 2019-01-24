f63a1ee0164c63e354d61c38d7ec6893d37cb688
Ryan Parker <rparker@indeed.com>
2019-01-24 15:44:02 -0600

    re-enable sqlite

--- proj/settings/local_dev.py
+++ proj/settings/local_dev.py
@@ -10,11 +10,11 @@ CACHE_MIDDLEWARE_SECONDS = 0 # Really just prevents us form caching in dev envir
 ALLOWED_HOSTS += ['localhost', 'iniav2-qa.herokuapp.com']
 
 # SQLite, uncomment to use this instead of mysql.
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
+
